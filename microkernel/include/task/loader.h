aca89f6837e2acc097563634f66aadbbf19b0669
Gonzalo J. Carracedo <BatchDrake@gmail.com>
2014-01-31 22:58:24 +0100

    First release of misc binary loader API

---
+++ microkernel/include/task/loader.h
@@ -0,0 +1,62 @@
+/*
+ *    Prototypes and declarations for the misc executable loader API
+ *    Copyright (C) 2014 Gonzalo J. Carracedo <BatchDrake@gmail.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#ifndef _TASK_LOADER_H
+#define _TASK_LOADER_H
+
+#include <types.h>
+#include <mm/vm.h>
+
+#define LOADER_MAX 16
+
+struct loader
+{
+  const char *name;
+  const char *desc;
+
+  void *        (*open)    (void *, uint32_t);
+  busword_t     (*entry)   (void *);
+  int           (*walkseg) (void *, struct vm_space *, int (*) (struct vm_space *, int, int, busword_t, busword_t));
+  void          (*close)   (void *);
+};
+
+typedef struct
+{
+  struct loader   *loader;
+  struct vm_space *target_space;
+  void            *exec_base;
+  uint32_t         exec_size;
+  void            *opaque;
+}
+loader_handle;
+
+INLINE BOOL
+loader_is_usable (struct loader *loader)
+{
+  return loader->open    != NULL &&
+         loader->entry   != NULL &&
+         loader->walkseg != NULL &&
+         loader->close   != NULL;
+}
+
+struct loader *loader_register (const char *, const char *);
+loader_handle *loader_open_exec (struct vm_space *, void *, uint32_t);
+busword_t loader_get_exec_entry (loader_handle *);
+int loader_walk_exec (loader_handle *, int (*) (struct vm_space *, int, int, busword_t, busword_t));
+void loader_close_exec (loader_handle *);
+#endif /* _TASK_LOADER_H */
---
+++ microkernel/loader.c
@@ -0,0 +1,99 @@
+/*
+ *    Implements the misc executable loader API
+ *    Copyright (C) 2014 Gonzalo J. Carracedo <BatchDrake@gmail.com>
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <types.h>
+#include <mm/regions.h>
+#include <mm/spalloc.h>
+
+#include <task/loader.h>
+
+#include <string.h>
+#include <util.h>
+
+static struct loader loader_list[LOADER_MAX];
+static int           loader_count;
+
+/* TODO: lock */
+struct loader *
+loader_register (const char *name, const char *desc)
+{
+  if (loader_count >= LOADER_MAX)
+    return NULL;
+
+  loader_list[loader_count].name = name;
+  loader_list[loader_count].desc = desc;
+
+  return loader_list + loader_count++;
+}
+
+/* TODO: add error status */
+loader_handle *
+loader_open_exec (struct vm_space *space, void *exec_start, uint32_t exec_size)
+{
+  int i;
+  loader_handle *handle;
+  void *opaque;
+  
+  for (i = 0; i < loader_count; ++i)
+    if (loader_is_usable (loader_list + i))
+      if ((opaque = (loader_list[i].open) (exec_start, exec_size)) != KERNEL_INVALID_POINTER)
+      {
+        if (PTR_UNLIKELY_TO_FAIL (handle = spalloc (sizeof (loader_handle))))
+        {
+          error ("Not enough memory to allocate loader handle!\n");
+
+          (loader_list[i].close) (opaque);
+
+          return KERNEL_INVALID_POINTER;
+        }
+
+        handle->loader       = loader_list + i;
+        handle->target_space = space;
+        handle->exec_base    = exec_start;
+        handle->exec_size    = exec_size;
+        handle->opaque       = opaque;
+        
+        return handle;
+      }
+
+  return KERNEL_INVALID_POINTER;
+}
+
+busword_t 
+loader_get_exec_entry (loader_handle *handle)
+{
+  return (handle->loader->entry) (handle->opaque);
+}
+
+/* Walk exec gives information about type & flags of the computed segment */
+int
+loader_walk_exec (loader_handle *handle, int (*callback) (struct vm_space *, int, int, busword_t, busword_t))
+{
+  return (handle->loader->walkseg) (handle->opaque, handle->target_space, callback);
+}
+
+void
+loader_close_exec (loader_handle *handle)
+{
+  (handle->loader->close) (handle->opaque);
+
+  spfree (handle);
+}
+
+
+
